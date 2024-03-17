0683f36af3f0314b77a0a440523913b18653ea1e
luping.bi <2631132715@qq.com>
2024-03-17 19:44:11 +0800

    style: 使用 ProjectActivity 消除 idea 警告

--- src/main/java/com/didalgo/intellij/chatgpt/core/StartUpActivity.java
+++ src/main/java/com/didalgo/intellij/chatgpt/core/StartUpActivity.java
@@ -4,17 +4,25 @@
  */
 package com.didalgo.intellij.chatgpt.core;
 
-import com.intellij.openapi.project.Project;
 import com.didalgo.intellij.chatgpt.settings.OpenAISettingsState;
 import com.didalgo.intellij.chatgpt.ui.action.editor.ActionsUtil;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.ProjectActivity;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class StartUpActivity implements StartupActivity {
+/**
+ * 动态注册一些 action(explain 、FindBug)
+ */
+public class StartUpActivity implements ProjectActivity {
 
+    @Nullable
     @Override
-    public void runActivity(@NotNull Project project) {
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         OpenAISettingsState.getInstance();
         ActionsUtil.refreshActions();
+        return null;
     }
 }
